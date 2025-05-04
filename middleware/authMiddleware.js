f34dc388f467952558352aefbe6d4033eb0f1bf8
japri099 <injapri@gmail.com>
2025-05-05 05:43:21 +0700

    feat: Implementasi Logika Backend Profile Admin

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token tidak tersedia' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token tidak valid' });
+    req.user = user;
+    next();
+  });
+}
+
+function authorizeRole(...allowedRoles) {
+  return (req, res, next) => {
+    const userRole = req.user.role;
+    if (!allowedRoles.includes(userRole)) {
+      return res.status(403).json({ message: 'Akses ditolak' });
+    }
+    next();
+  };
+}
+
+module.exports = { authenticateToken, authorizeRole };
